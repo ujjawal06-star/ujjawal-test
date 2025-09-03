501a54b994efc8887f92c0d7d84f3fbb8232ae7b
ujjawal06-star <ujjwal9197us@gmail.com>
2025-09-03 21:32:54 +0530

    objects in js

---
+++ objects.js
@@ -0,0 +1,19 @@
+//object creation
+const car= {
+    name:"suzuki",
+    model:"first",
+    color:"white"
+};
+console.log(car.name);
+console.log(car.model);
+console.log(car.color);
+
+// person detail
+const person ={
+    name: "ujjawal",
+    age:19,
+    city:"ajmer"
+};
+person.city="jaipur";
+person.country  = "india";
+console.log(person)
